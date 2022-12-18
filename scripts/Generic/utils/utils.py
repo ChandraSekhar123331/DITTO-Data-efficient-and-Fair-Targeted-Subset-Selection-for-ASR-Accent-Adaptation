62ff223c3a4acb1656b1e184a54751d1d87405b0
iMayK <maykat2017@gmail.com>
2022-12-18 20:40:42 +0530

    add sample_random functionality

--- scripts/Generic/utils/utils.py
+++ scripts/Generic/utils/utils.py
@@ -1,5 +1,7 @@
-import os
 import json
+import os
+
+import numpy as np
 
 
 def sample_greedy(lines, required_duration):
@@ -7,10 +9,31 @@ def sample_greedy(lines, required_duration):
     index = 0
     selected_lines = []
 
-
     while selected_duration < required_duration:
         selected_lines.append(lines[index])
         selected_duration += lines[index]["duration"]
+        index += 1
+
+    return selected_lines
+
+
+def shuffle(lines, rng):
+    indices = list(range(len(lines)))
+    rng.shuffle(indices)  # shuffles indices in-place
+    return [lines[index] for index in indices]
+
+
+def sample_random(input_lines, required_duration, seed):
+
+    rng = np.random.default_rng(seed=seed)
+    shuffled_lines = shuffle(input_lines, rng)
+    selected_duration = 0
+    index = 0
+    selected_lines = []
+
+    while selected_duration < required_duration:
+        selected_lines.append(shuffled_lines[index])
+        selected_duration += shuffled_lines[index]["duration"]
         index += 1
 
     return selected_lines
@@ -26,4 +49,3 @@ def dump_lines(lines, OUTPUT_JSON_PATH):
 
 def read_lines(INPUT_JSON_PATH):
     return [json.loads(line.strip()) for line in open(INPUT_JSON_PATH)]
-    
