1454de3f0c5d78c79ee6707e73ec235e96bf3953
iMayK <maykat2017@gmail.com>
2023-01-09 07:07:48 +0530

    Added multi server support and bugfix

--- scripts/Generic/utils/dataset.py
+++ scripts/Generic/utils/dataset.py
@@ -15,10 +15,18 @@ def all_datasets():
     return tuple(_.name for _ in Dataset)
 
 
-def get_path(data_obj):
-    HOME = HOME_PATH()
+def all_features():
+    return ("MFCC", "MFCC_unit_norm", "w2v2_Ftill10_768-512-256")
+
+
+def all_servers():
+    return ("SWARA", "DRISTI", "AIRAWAT", "DHVANI")
+
+
+def get_path(data_obj, server):
+    HOME = HOME_PATH(server=server)
     DATASET = DATASET_PATH(data_obj)
-    WAV = WAV_PATH(data_obj)
+    WAV = WAV_PATH(data_obj, server=server)
     return HOME, DATASET, WAV
 
 
@@ -32,34 +40,34 @@ def get_accent(line, dataset_name):
         )
     elif data_obj is Dataset.L2:
         accent_map = {
-            "ABA": "Arabic",
-            "SKA": "Arabic",
-            "YBAA": "Arabic",
-            "ZHAA": "Arabic",
-            "BWC": "Chinese",
-            "LXC": "Chinese",
-            "NCC": "Chinese",
-            "TXHC": "Chinese",
-            "ASI": "Hindi",
-            "RRBI": "Hindi",
-            "SVBI": "Hindi",
-            "TNI": "Hindi",
-            "HJK": "Korean",
-            "HKK": "Korean",
-            "YDCK": "Korean",
-            "YKWK": "Korean",
-            "EBVS": "Spanish",
-            "ERMS": "Spanish",
-            "MBMPS": "Spanish",
-            "NJS": "Spanish",
-            "HQTV": "Vietnamese",
-            "PNV": "Vietnamese",
-            "THV": "Vietnamese",
-            "TLV": "Vietnamese",
+            "ABA": "arabic",
+            "SKA": "arabic",
+            "YBAA": "arabic",
+            "ZHAA": "arabic",
+            "BWC": "chinese",
+            "LXC": "chinese",
+            "NCC": "chinese",
+            "TXHC": "chinese",
+            "ASI": "hindi",
+            "RRBI": "hindi",
+            "SVBI": "hindi",
+            "TNI": "hindi",
+            "HJK": "korean",
+            "HKK": "korean",
+            "YDCK": "korean",
+            "YKWK": "korean",
+            "EBVS": "spanish",
+            "ERMS": "spanish",
+            "MBMPS": "spanish",
+            "NJS": "spanish",
+            "HQTV": "vietnamese",
+            "PNV": "vietnamese",
+            "THV": "vietnamese",
+            "TLV": "vietnamese",
         }
 
         speaker = re.search("/l2_new/([A-Za-z_\-]+)/", line["audio_filepath"]).group(1)
-        print(speaker)
+        # print(speaker)
         accent = accent_map[speaker]
     elif data_obj is Dataset.MCV:
         accent = line["accent"]
@@ -93,7 +101,6 @@ def get_all_accents(data_obj):
             "ireland",
             "philippines",
             "scotland",
-            "southatlandtic",
             "us",
         )
     else:
@@ -115,24 +122,54 @@ def DATASET_PATH(data_obj):
     return PATH
 
 
-def HOME_PATH():
-    PATH = os.path.join(
-        "/home", "mayank", "MTP", "begin_again", "Error-Driven-ASR-Personalization"
-    )
-    return PATH
-
-
-def WAV_PATH(data_obj):
-    if data_obj is Dataset.INDIC:
-        PATH = os.path.join("")
-    elif data_obj is Dataset.L2:
-        PATH = os.path.join("")
-    elif data_obj is Dataset.MCV:
-        PATH = os.path.join("mozilla", "cv-corpus-7.0-2021-07-21/en/wav/")
+def HOME_PATH(server):
+    if server == "SWARA":
+        PATH = os.path.join(
+            "/home", "mayank", "MTP", "begin_again", "Error-Driven-ASR-Personalization"
+        )
+    elif server == "AIRAWAT":
+        PATH = os.path.join(
+            "/home", "mohith", "mayank", "Error-Driven-ASR-Personalization"
+        )
+    elif server == "DRISTI":
+        PATH = os.path.join(
+            "/home", "aman", "mayank", "Error-Driven-ASR-Personalization"
+        )
+    elif server == "DHVANI":
+        PATH = os.path.join(
+            "/home", "mayur", "mayank", "Error-Driven-ASR-Personalization"
+        )
     else:
-        raise ValueError(f"{data_obj.name} is not a valid Dataset")
-
+        print(server)
+        raise ValueError(f"{server} is not a valid Server name")
     return PATH
+
+
+def WAV_PATH(data_obj, server):
+    if server == "SWARA":
+        prefix = os.path.join("/home", "mayank", "MTP", "begin_again")
+    elif server == "AIRAWAT":
+        prefix = os.path.join("/home", "mohith", "mayank")
+    elif server == "DRISTI":
+        prefix = os.path.join("/home", "aman", "mayank")
+    elif server == "DHVANI":
+        prefix = os.path.join("/home", "mayur", "mayank")
+    else:
+        raise ValueError(f"{server} is not a valid Server name")
+    # if data_obj is Dataset.INDIC:
+    #     PATH = os.path.join(
+    #         "/home",
+    #         "aman",
+    #         "mayank",
+    #     )
+    # elif data_obj is Dataset.L2:
+    #     PATH = os.path.join("")
+    # elif data_obj is Dataset.MCV:
+    #     PATH = os.path.join("mozilla", "cv-corpus-7.0-2021-07-21/en/wav/")
+    # else:
+    #     raise ValueError(f"{data_obj.name} is not a valid Dataset")
+
+    return prefix
 
 
 def BUDGET_TO_DURATION(budget):
@@ -141,7 +178,7 @@ def BUDGET_TO_DURATION(budget):
 
 def update_config(config):
     config["HOME_PATH"], config["DATASET_PATH"], config["WAV_PATH"] = get_path(
-        Dataset[config["dataset"]]
+        Dataset[config["dataset"]], server=config["server"]
     )
     config["FULL_DATASET_PATH"] = os.path.join(
         config["HOME_PATH"], config["DATASET_PATH"]
@@ -161,9 +198,20 @@ if __name__ == "__main__":
     print(Dataset(2) is Dataset["L2"])
     print(Dataset(2) is Dataset.L2)
 
-    print(get_path(Dataset["INDIC"]))
-    print(get_path(Dataset["L2"]))
-    print(get_path(Dataset["MCV"]))
+    print(get_path(Dataset["INDIC"], "SWARA"))
+    print(get_path(Dataset["INDIC"], "DRISTI"))
+    print(get_path(Dataset["INDIC"], "AIRAWAT"))
+    print(get_path(Dataset["INDIC"], "DHVANI"))
+
+    print(get_path(Dataset["L2"], "SWARA"))
+    print(get_path(Dataset["L2"], "DRISTI"))
+    print(get_path(Dataset["L2"], "AIRAWAT"))
+    print(get_path(Dataset["L2"], "DHVANI"))
+    
+    print(get_path(Dataset["MCV"], "SWARA"))
+    print(get_path(Dataset["MCV"], "DRISTI"))
+    print(get_path(Dataset["MCV"], "AIRAWAT"))
+    print(get_path(Dataset["MCV"], "DHVANI"))
 
     # print(get_data_path(Dataset['CORAAL'])) # Will raise an error.
 
